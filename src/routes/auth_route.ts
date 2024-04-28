4246839d9adb2d7c046496312b0ed86e9f485546
vuongqtvn <quocvuongta1023@gmail.com>
2024-04-28 08:30:36 +0700

    setup

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -5,6 +5,7 @@ const authRoute = (route: Router) => {
   route.route("/auth/register").post(authController.register);
   route.route("/auth/login").post(authController.login);
   route.route("/auth/me").get(authController.getMe);
+  route.route("/auth/refresh_token").get(authController.refreshToken);
 };
 
 export default authRoute;
